5a178a59087afe5be51261b204314b26c734d2db
Samuel <slanguet@protonmail.com>
2022-08-19 10:33:20 +0200

    Update index.php
     Exemple avec la récupération sous forme de tableau des fonctions susmentionnées, recherche à partir du n-ième caractère

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Regex</title>
 </head>
 <body>
-<h1>Je fais du Regex !</h1>
+<h1>Je fais des Regex</h1>
 <?php
-$masque = '/m/';
-$chaine = 'mere michel';
+
+$masque = '/mer/';
+$chaine = 'la mere michel va à la mer';
 $match = [];
 $match_all = [];
 
-preg_match($masque,$chaine,$match);
-preg_match_all($masque,$chaine,$match_all);
+preg_match($masque, $chaine, $match);
+preg_match_all($masque, $chaine, $match_all);
 
 var_dump($match);
 echo "<br />";
@@ -23,11 +24,18 @@ echo "<br />";
 var_dump($match_all);
 echo "<br />";
 
+$match_all_2 = [];
+$valeur = preg_match_all($masque, $chaine, $match_all_2, PREG_PATTERN_ORDER, 3);
+echo "<p>Il y a $valeur fois la lettre recherchée</p>";
+
+
 if (preg_match($masque, $chaine)) {
-    echo "<p>j'ai trouvé l'occurence</p>";
-    echo "<p>Le caractère recherché est présent : " . preg_match_all($masque, $chaine) . " fois.</p>";
-} else {
-    echo "<p>je n'ai rien trouvé</p>";
+    echo "<p>J'ai trouvé l'occurence</p>";
+    echo "<p>Le caractère recherché est présente : " . preg_match_all($masque, $chaine) . " fois</p>";
+}
+
+else {
+    echo "<p>Je n'ai rien trouvé</p>";
 }
 
 ?>
