2ed6bfeffcfc29c7755426f43ee1a2dcf4361061
Samuel <slanguet@protonmail.com>
2022-08-19 11:49:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,48 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Regex</title>
+    <title>Document</title>
+    <style>body {background-color: lightslategrey}</style>
 </head>
 <body>
-<h1>Je fais des Regex</h1>
+<h1>Je fais du Regex !</h1>
 <?php
+$masque = '/Michel/';
+$tableau = ['Michel','Robert', 'Arthur', 'Jean-Michel'];
+$chaine = 'Bonjour, je m\'appelle Michel';
 
-$masque = '/mer/';
-$chaine = 'la mere michel va à la mer';
-$match = [];
-$match_all = [];
+// preg_replace() renvoie la chaîne de départ s'il n'y de correspondance, alors preg_filter() renvoie la valeur null
+$valeur = preg_filter($masque, 'Robert', $chaine);
+echo "<p>$valeur</p>";
 
-preg_match($masque, $chaine, $match);
-preg_match_all($masque, $chaine, $match_all);
+// Exemple avec la fonction preg_grep() qui permet une recherche sur un tableau
+$resultat = preg_grep($masque,$tableau);
 
-var_dump($match);
+var_dump($resultat);
+
+$masque1 = "/[aeiouy]/";
+$masque2 = "/j[aeiouy]/";
+$masque3 = "/[aeiouy][aeiouy]/";
+$masque4 = "/[aeiouy][aeiouy][aeiouy]/";
+$chaine = "je m'appelle Michel et je fais des tableaux en PHP au bord de l'eau, biiiiiiitch";
+
+preg_match_all($masque1, $chaine, $tableau1);
+preg_match_all($masque2, $chaine, $tableau2);
+preg_match_all($masque3, $chaine, $tableau3);
+preg_match_all($masque4, $chaine, $tableau4);
+
+var_dump($tableau1);
 echo "<br />";
-
-var_dump($match_all);
+var_dump($tableau2);
 echo "<br />";
+var_dump($tableau3);
+echo "<br />";
+var_dump($tableau4);
 
-$match_all_2 = [];
-$valeur = preg_match_all($masque, $chaine, $match_all_2, PREG_PATTERN_ORDER, 3);
-echo "<p>Il y a $valeur fois la lettre recherchée</p>";
 
-
-if (preg_match($masque, $chaine)) {
-    echo "<p>J'ai trouvé l'occurence</p>";
-    echo "<p>Le caractère recherché est présente : " . preg_match_all($masque, $chaine) . " fois</p>";
-}
-
-else {
-    echo "<p>Je n'ai rien trouvé</p>";
-}
 
 ?>
 </body>
